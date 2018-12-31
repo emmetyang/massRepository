44f6184f31c96e2ebe1e4dcf8d571154aeab7da5
yanghaishan <@qq123456>
2018-12-31 17:46:33 +0800

    提交改变的version

---
+++ src/test/java/com/imooc/SellApplicationTests.java
@@ -0,0 +1,20 @@
+package com.imooc;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		//Assert.assertNotNull(); 测试常用，通过引入
+		//相应的测试类进行计算
+	}
+
+}
